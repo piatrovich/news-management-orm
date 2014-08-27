c436d49f8129525fb571bd6e03679a25098227fb
Dzmitry Piatrovich <piatrovich@live.ru>
2014-08-28 00:27:57 +0300

    Enhanced PagingAndSortingRepository abstraction

--- src/main/java/com/epam/lab/news/data/repo/PagingAndSortingRepository.java
+++ src/main/java/com/epam/lab/news/data/repo/PagingAndSortingRepository.java
@@ -4,20 +4,10 @@ import com.epam.lab.news.data.bean.Page;
 
 import java.util.List;
 
-public interface PagingAndSortingRepository<T> extends CRUDRepository<T> {
+public interface PagingAndSortingRepository<E> extends CRUDRepository<E> {
 
-    public List<T> firstPage(Long size);
+    List<E> page(Page page, Long[]...args);
 
-    public List<T> page(Page page);
-
-    public Long pageCount(Long pageSize);
-
-    /*public List<T> nextPage(Page page);
-
-    public boolean hasNextPage(Page page);
-
-    public List<T> previousPage(Page page);
-
-    public boolean hasPreviousPage(Page page);*/
+    Long pageCount(Long pageSize, Long[]...args);
 
 }
