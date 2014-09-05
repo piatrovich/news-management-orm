c7489989da8d906265ad7dc77deeccd812327c77
Dzmitry Piatrovich <piatrovich@live.ru>
2014-09-05 16:45:02 +0300

    Documented CRUD repository abstraction

--- src/main/java/com/epam/lab/news/data/repo/CRUDRepository.java
+++ src/main/java/com/epam/lab/news/data/repo/CRUDRepository.java
@@ -2,18 +2,57 @@ package com.epam.lab.news.data.repo;
 
 import java.util.List;
 
+/**
+ * Defines abstraction for CRUD operations with entities
+ *
+ * @author Dzmitry Piatrovich
+ * @param <E> Entity class
+ */
 public interface CRUDRepository<E> {
 
+    /**
+     * Return list of all available entities
+     *
+     * @return List of all entities
+     */
     List<E> all();
 
+    /**
+     * Returns single entity by ID
+     *
+     * @param id Entity ID
+     * @return Entity
+     */
     E one(Long id);
 
+    /**
+     * Saves and returns object with the same values as in table
+     *
+     * @param entity Entity
+     * @return Saved entity
+     */
     E save(E entity);
 
+    /**
+     * Returns count of all available objects
+     *
+     * @return Long number
+     */
     Long count();
 
+    /**
+     * Deletes object from DB
+     *
+     * @param entity Application bean
+     */
     void delete(E entity);
 
+    /**
+     * Checks object existing in DB
+     *
+     * @param id Entity ID
+     * @return True if exist, otherwise false
+     */
     boolean exists(Long id);
 
 }
